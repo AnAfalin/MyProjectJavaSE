87b9eaa44c926ece1e777693ece542e821a47795
Anastasia <an.afalin@yandex.ru>
2022-04-08 19:30:32 +0300

    add implementations of sorting tasks from Additional package Sort

---
+++ 11.Additional/src/Sort/BubbleSort.java
@@ -0,0 +1,33 @@
+package Sort;
+
+//сортировка пузырьком
+
+public class BubbleSort {
+
+
+    public static void main(String[] args) {
+        int[] array = {10, 5, 8, 2};
+        sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void sort (int[] array) { //O(n^2)
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+
+                if(array[i] > array[j]) {
+                    int tmp = array[j];
+                    array[j] = array[i];
+                    array[i] = tmp;
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
---
+++ 11.Additional/src/Sort/QuickSort.java
@@ -0,0 +1,48 @@
+package Sort;
+
+import java.util.Arrays;
+
+//быстрая сортировка
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = {8, 0, 4, 7, 3, 7, 10, 12, -3 };
+        System.out.println("Было");
+        System.out.println(Arrays.toString(array));
+        quickSort(array, 0, array.length-1);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void quickSort(int[] array, int beginElement, int endElement) {
+        //выбираем опорный элемент
+        int middle = beginElement + (endElement - beginElement) / 2;
+        int support = array[middle];
+
+        //делим массив на подмассивы, которые больше и меньше опорного элемента
+        int i = beginElement, j = endElement;
+        while (i<=j){
+            while (array[i] < support) {
+                i++;
+            }
+
+            while (array[j] > support) {
+                j--;
+            }
+
+            if(i<=j) {//меняем местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (beginElement < j)
+            quickSort(array, beginElement, j);
+
+        if (endElement > i)
+            quickSort(array, i, endElement);
+    }
+
+}
