041ced357eb26fdc36c66591a1edd31b2bcc3ad0
Anastasia <an.afalin@yandex.ru>
2022-04-12 14:23:33 +0300

    update solutions task2 from part1 from Generics

--- 4.Generics/src/part1/task2/Account.java
+++ 4.Generics/src/part1/task2/Account.java
@@ -3,12 +3,22 @@ package part1.task2;
 class Account<T> {
     private T balance;
 
-    public T getBalance() {
-        return balance;
-    }
-
     public void setBalance(T balance) {
         this.balance = balance;
     }
 
+    public double getBalance() {
+        return Double.parseDouble(balance.toString());
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        Account<Integer> acc = new Account<>();
+        acc.setBalance(500);
+        System.out.println(acc.getBalance());
+
+
+    }
 }
