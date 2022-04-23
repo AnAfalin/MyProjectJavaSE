23e98602e93953a17ecb0702a1695dffc03425b7
Anastasia <an.afalin@yandex.ru>
2022-04-23 21:07:08 +0300

    update solution task1 from StreamInputOutput.part1

--- 6.StreamInputOutput/src/part1/task1/Solution.java
+++ 6.StreamInputOutput/src/part1/task1/Solution.java
@@ -1,28 +1,28 @@
 package part1.task1;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        byte[] arr = str.getBytes();
-        OutputStream outputStream = null;
-        try {
-            outputStream = new FileOutputStream("Task1.txt");
+
+        try (Scanner scanner = new Scanner(System.in);
+             OutputStream outputStream = new FileOutputStream("Task1.txt")) {
+            String str = scanner.nextLine();
+            byte[] arr = str.getBytes();
             outputStream.write(arr);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+        }
+
+        try (InputStream is = new FileInputStream("Task1.txt")) {
+            int c;
+            while ((c = is.read()) != -1) {
+                System.out.print((char) (c));
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
