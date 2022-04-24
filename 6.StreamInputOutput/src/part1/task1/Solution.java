8f32a2ab413d8cad0c30f9066c17b3b086b0aec7
Anastasia <an.afalin@yandex.ru>
2022-04-24 12:27:57 +0300

    update solution task1 from StreamInputOutput.part1

--- 6.StreamInputOutput/src/part1/task1/Solution.java
+++ 6.StreamInputOutput/src/part1/task1/Solution.java
@@ -1,28 +1,23 @@
 package part1.task1;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
 
         try (Scanner scanner = new Scanner(System.in);
-             OutputStream outputStream = new FileOutputStream("Task1.txt")) {
+             OutputStream outputStream = new FileOutputStream("6.StreamInputOutput/src/part1/task1/Task1.txt");
+             InputStream is = new FileInputStream("6.StreamInputOutput/src/part1/task1/Task1.txt")) {
             String str = scanner.nextLine();
             byte[] arr = str.getBytes();
             outputStream.write(arr);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (InputStream is = new FileInputStream("Task1.txt")) {
             int c;
             while ((c = is.read()) != -1) {
                 System.out.print((char) (c));
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
