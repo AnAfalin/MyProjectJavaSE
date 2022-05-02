1a04a8945619b0f50215b15a6309dbdd3a2dcf3c
Anastasia <an.afalin@yandex.ru>
2022-05-02 14:52:18 +0300

    add solution task11 from StreamInputOutput.part1

---
+++ 6.StreamInputOutput/src/part1/task11/Solution.java
@@ -0,0 +1,27 @@
+package part1.task11;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("6.StreamInputOutput/src/part1/task11/LargeNumber.txt"))) {
+            String str = bufferedReader.readLine();
+            int[] arrNum = new int[10];
+            for (int i = 0; i < str.length(); i++) {
+                int number = Integer.parseInt(String.valueOf(str.charAt(i)));
+                arrNum[number] += 1;
+            }
+
+            System.out.println("В считанном файле содержалось большое число: " + str + ", в котором:");
+            for (int i = 0; i < arrNum.length; i++) {
+                System.out.println("кол-во вхождений цифры " + i + " составляет " + arrNum[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
