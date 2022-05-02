5c48295ca6e0bcba4b99f72afd414d6928238ee1
Anastasia <an.afalin@yandex.ru>
2022-05-02 13:25:00 +0300

    update task7 from StreamInputOutput.part1

--- 6.StreamInputOutput/src/part1/task6/Solution.java
+++ 6.StreamInputOutput/src/part1/task6/Solution.java
@@ -1,7 +1,34 @@
 package part1.task6;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = i*i + j*j;
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
 
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task7/matrix.txt"))){
+            String str = "";
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    str += matrix[i][j] + " ";
+
+                }
+                bufferedWriter.write(str);
+                str = "";
+                bufferedWriter.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
