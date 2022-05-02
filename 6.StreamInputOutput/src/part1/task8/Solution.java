d43abf4523318d585640bd891bf051b2c99279e1
Anastasia <an.afalin@yandex.ru>
2022-05-02 13:06:39 +0300

    update task8 from StreamInputOutput.part1

--- 6.StreamInputOutput/src/part1/task8/Solution.java
+++ 6.StreamInputOutput/src/part1/task8/Solution.java
@@ -6,29 +6,36 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("6.StreamInputOutput/src/part1/task8/Words"));
-        BufferedWriter bufferedWriter1 = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task8/first.txt"));
-        BufferedWriter bufferedWriter2 = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task8/second.txt"));
-        BufferedWriter bufferedWriter3 = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task8/third.txt")))
-        {
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new FileReader("6.StreamInputOutput/src/part1/task8/Words"));
+                BufferedWriter bufferedWriter1 = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task8/first.txt"));
+                BufferedWriter bufferedWriter2 = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task8/second.txt"));
+                BufferedWriter bufferedWriter3 = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part1/task8/third.txt"))
+        ) {
             List<String> list = new ArrayList<>();
+
             String str;
-            int i = 0;
-                while((str = bufferedReader.readLine()) != null){
-                    list.add(i, str);
-                    i++;
-                }
-                String string;
+
+            while ((str = bufferedReader.readLine()) != null) {
+                list.add(str);
+            }
+
             for (int j = 0; j < list.size(); j++) {
-                string = list.get(j);
-                if(string.matches("\\b[A-ZА-Я][a-zа-я]*")){
-                    bufferedWriter1.write(string);
-                }
-                if(string.matches("\\b[A-Za-zА-Яа-я]{5}\\b")){
-                    bufferedWriter2.write(string);
-                }
-                if(string.matches("\\b[^0-9A-ZА-Я\\s]+\\b")){
-                    bufferedWriter3.write(string);
+
+                String[] words = list.get(j).split(" ");
+                for (String word : words) {
+
+                    word = word.replaceAll("[.,:;-]", "");
+
+                    if (word.matches("[A-ZА-Я][a-zа-я]*")) {
+                        bufferedWriter1.write(word + "\n");
+                    }
+                    if (word.matches("[A-Za-zА-Яа-я]{5}")) {
+                        bufferedWriter2.write(word + "\n");
+                    }
+                    if (word.matches("[^0-9A-ZА-Я\\s]+")) {
+                        bufferedWriter3.write(word + "\n");
+                    }
                 }
             }
         } catch (IOException e) {
