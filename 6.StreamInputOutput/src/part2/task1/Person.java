acc70ec8da4eda089281730370259d31a3e754dc
Anastasia <an.afalin@yandex.ru>
2022-05-04 20:45:43 +0300

    update solution task1 from StreamInputOutput.part2

--- 6.StreamInputOutput/src/part2/task1/Person.java
+++ 6.StreamInputOutput/src/part2/task1/Person.java
@@ -4,7 +4,10 @@ import java.io.Serial;
 import java.io.Serializable;
 
 public class Person implements Serializable {
-            
+
+    @Serial
+    private static final long serialVersionUID = -779353353294981746L;
+
     private String name;
     private int age;
 
--- 6.StreamInputOutput/src/part2/task1/Solution.java
+++ 6.StreamInputOutput/src/part2/task1/Solution.java
@@ -8,15 +8,24 @@ public class Solution {
         Person person2 = new Person("Sara", 20);
         Person person3 = new Person("Tom", 30);
 
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("6.StreamInputOutput/src/part2/task1/Person.bin"));
-             ObjectInputStream ois = new ObjectInputStream(new FileInputStream("6.StreamInputOutput/src/part2/task1/Person.bin"))){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("6.StreamInputOutput/src/part2/task1/Person.bin"))){
             oos.writeObject(person1);
             oos.writeObject(person2);
             oos.writeObject(person3);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("6.StreamInputOutput/src/part2/task1/Person.bin"))){
+
             Person savePerson = (Person) ois.readObject();
-            Person savePerson1 = (Person) ois.readObject();
+            System.out.println(savePerson);
             Person savePerson2 = (Person) ois.readObject();
+            System.out.println(savePerson2);
+            Person savePerson3 = (Person) ois.readObject();
+            System.out.println(savePerson3);
+
 
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
