59508ad354376b428463aaeeb91454af940b0b3b
Anastasia <an.afalin@yandex.ru>
2022-05-05 14:51:24 +0300

    update task from ArrayListCollection.part1 from Collections

--- 7.Collections/src/ArrayListCollection/part1/ArrayList.java
+++ 7.Collections/src/ArrayListCollection/part1/ArrayList.java
@@ -1,17 +1,78 @@
 package ArrayListCollection.part1;
 
-public class ArrayList<T> {
+public class ArrayList<E> {
     private int size;
     private int capacity;
-    private T[] array;
+    private E[] array;
 
     @SuppressWarnings("unchecked")
     public ArrayList() {
-        array = (T[]) new Object[capacity];
+        capacity = 10;
+        size = 0;
+        array = (E[]) new Object[capacity];
     }
 
+    //метод добавления в конец
+    public void add(E el) {
+        if (size + 1 >= capacity) {
+            grow();
+        }
+        add(el, size, array);
+        size++;
+    }
+
+    //метод добавления по любому индексу
+    public void add(E el, int index) {
+        if (size + 1 >= capacity) {
+            grow();
+        }
+        add(el, index, array);
+        size++;
+    }
+
+    //метод добавления в начало
+    public void addFirst(E el) {
+        if (size + 1 >= capacity) {
+            grow();
+        }
+        add(el, 0, array);
+    }
+
+    //метод добавления - основной
+    private void add(E el, int index, E[] array) {
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = el;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void grow() {
+        Object[] newArr = new Object[(int) (capacity * 1.5 + 1)];
+        System.arraycopy(array, 0, newArr, 0, array.length);
+        array = (E[]) newArr;
+    }
+
+    //метод вывода на консоль списка
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
 
 
-
-
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.print();
+        list.addFirst(88);
+        list.add(88, 2);
+        list.print();
+    }
 }
