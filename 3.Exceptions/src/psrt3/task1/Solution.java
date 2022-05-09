1c6c468d327d2f9fd65bc4ad45c8484710796e8c
Anastasia <an.afalin@yandex.ru>
2022-05-09 23:38:43 +0300

    add solution task1 from part3.Exception

---
+++ 3.Exceptions/src/psrt3/task1/Solution.java
@@ -0,0 +1,35 @@
+package psrt3.task1;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                StringBuilder expression = new StringBuilder(scanner.nextLine());
+                double num1 = Double.parseDouble(expression.substring(0, expression.indexOf(" ")));
+                expression.delete(0, expression.indexOf(" ") + 1);
+                char operation = expression.charAt(0);
+                expression.delete(0, 2);
+                double num2 = Double.parseDouble(expression.substring(0));
+                double result = 0.0;
+                if (operation == '*') {
+                    result = num1 * num2;
+                } else if (operation == '/') {
+                    result = num1 / num2;
+                } else if (operation == '+') {
+                    result = num1 + num2;
+                } else if (operation == '-') {
+                    result = num1 - num2;
+                }
+                System.out.println(result);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка. Повторите правильный ввод строки выражения в формате: \"число [operation] число\"");
+            }
+        } while (true);
+    }
+}
+
