1056ddfb1ffeec6fb7d58539c4ed38277d2f6e1a
Anastasia <an.afalin@yandex.ru>
2022-05-12 10:31:17 +0300

    update task1 from part1/queueCollection/Collections

--- 7.Collections/src/queueCollection/part1/task1/Queue.java
+++ 7.Collections/src/queueCollection/part1/task1/Queue.java
@@ -2,9 +2,11 @@ package queueCollection.part1.task1;
 
 /*
 Односторонняя очередь
+-head-  --- --- --- --- -tail-
  */
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 public class Queue<E> {
     private Object array[];
@@ -18,7 +20,11 @@ public class Queue<E> {
         array = new Object[capacity];
     }
 
-    //метод добавление в очередь
+    //метод добавляет элемент в конец очереди.
+    /*
+    Если элемент удачно добавлен, возвращает true
+    Если элемент равен null - бросается исключение NullPointerException
+     */
     public boolean offer(E element){
         isNullElement(element);
         if(tail + 1 >= capacity) {
@@ -31,7 +37,10 @@ public class Queue<E> {
         return true;
     }
 
-    //метод удаления из очереди
+    //метод возвращает элемент из начала очереди с удалением этого элемента.
+    /*
+    Если очередь пуста, возвращает значение null
+     */
     @SuppressWarnings("unchecked")
     public E poll(){
         if(isEmptyQueue()){
@@ -46,14 +55,61 @@ public class Queue<E> {
         return element;
     }
 
+    //метод возвращает элемент из начала очереди с удалением этого элемента.
+    /*
+    Если очередь пуста, генерирует исключение NoSuchElementException
+     */
+    @SuppressWarnings("unchecked")
+    public E remove(){
+        isEmptyQueueException();
+        E element = (E)array[head];
+        head++;
+        Object[] newArray = new Object[capacity];
+        System.arraycopy(array, head, newArray, 0, tail);
+        array = newArray;
+        head--;
+        return element;
+    }
+
+    //метод возвращает, но не удаляет, элемент из начала очереди.
+    /*
+    Если очередь пуста, генерирует исключение NoSuchElementException
+     */
+    @SuppressWarnings("unchecked")
+    public E element(){
+        isEmptyQueueException();
+        return (E)array[head];
+    }
+
+    //метод возвращает элемент из начала очереди без удаления этого элемента.
+    /*
+    Если очередь пуста, возвращает значение null
+     */
+    @SuppressWarnings("unchecked")
+    public E peek(){
+        if(isEmptyQueue()){
+            return null;
+        }
+        return (E)array[head];
+    }
+
+    //метод проверки элемента на значение null
     private void isNullElement(E element){
         if(element == null){
             throw new NullPointerException("Element is null");
         }
     }
 
+    //метод проверки очереди на непустоту
     private boolean isEmptyQueue(){
         return tail == head;
+    }
+
+    //метод проверки очереди на непустоту с генерацией исключения
+    private void isEmptyQueueException(){
+        if(tail == head){
+            throw new NoSuchElementException("Queue is empty");
+        }
     }
 
     @Override
--- 7.Collections/src/queueCollection/part1/task1/Test.java
+++ 7.Collections/src/queueCollection/part1/task1/Test.java
@@ -4,21 +4,35 @@ package queueCollection.part1.task1;
 public class Test {
     public static void main(String[] args) {
         Queue<Integer> queue = new Queue<>();
+
+        //проверка генерации ошибки при удалении элемента из пустой очереди
+        //queue.remove();
+
+        //проверка генерации ошибки при возвращении первого элемента из пустой очереди
+        //queue.element();
+
+        //проверка возвращения значения null из пустой очереди
+        System.out.println(queue.peek());
+
         queue.offer(5);
         queue.offer(10);
-        queue.offer(15);
         queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
 
         System.out.println(queue);
 
-        System.out.println(queue.poll());
-
-        System.out.println(queue);
-        queue.offer(25);
-
+        System.out.println("First element out " + queue.poll());
         System.out.println(queue);
 
-        System.out.println(queue.poll());
+        System.out.println("First element print, but doesn't out " + queue.element());
+        System.out.println(queue);
+
+        System.out.println("Add element in tail queue");
+        queue.offer(50);
+        System.out.println(queue);
+        queue.poll();
+        queue.poll();
         System.out.println(queue);
 
     }
