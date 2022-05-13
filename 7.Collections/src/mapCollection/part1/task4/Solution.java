d42707fd719dd7aa75623f40047b52f1e39b8f7a
Anastasia <an.afalin@yandex.ru>
2022-05-13 15:46:18 +0300

    add solution task4 from part1/mapCollection/Collections

---
+++ 7.Collections/src/mapCollection/part1/task4/Solution.java
@@ -0,0 +1,14 @@
+package mapCollection.part1.task4;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        HashMap<Integer, Double> hashMap = new HashMap<>();
+        hashMap.put(5, 25.0);
+        hashMap.put(2, 4.0);
+        hashMap.put(8, 64.0);
+        hashMap.put(3, 9.0);
+        System.out.println(hashMap);
+    }
+}
