9ac729919abbbc26a097823d49acca419a71ab4d
Anastasia <an.afalin@yandex.ru>
2022-05-14 00:08:40 +0300

    add solution task4 from part1/mapCollection/Collections

--- 7.Collections/src/mapCollection/part1/task4/Solution.java
+++ 7.Collections/src/mapCollection/part1/task4/Solution.java
@@ -4,11 +4,24 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        HashMap<Integer, Double> hashMap = new HashMap<>();
-        hashMap.put(5, 25.0);
-        hashMap.put(2, 4.0);
-        hashMap.put(8, 64.0);
-        hashMap.put(3, 9.0);
-        System.out.println(hashMap);
+        Map<Integer, Double> map = new HashMap<>();
+        map.put(5, 5.0*5.0);
+        map.put(55, 55.0*55.0);
+        map.put(2, 2.0*2.0);
+        map.put(23, 23.0*23.0);
+        map.put(44, 44.0*44.0);
+        map.put(10, 10.0*10.0);
+        map.put(3, 3.0*3.0);
+        System.out.println(map);
+
+        Integer[] arr = new Integer[map.size()];
+        map.keySet().toArray(arr);
+        Arrays.sort(arr);
+        LinkedHashMap<Integer, Double> linkedHashMap = new LinkedHashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            linkedHashMap.put(arr[i], map.get(arr[i]));
+        }
+        System.out.println(linkedHashMap);
+
     }
 }
