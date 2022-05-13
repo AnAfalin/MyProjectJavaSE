ea376008eb1d5a23433cd2bea5f0bd0281a31df3
Anastasia <an.afalin@yandex.ru>
2022-05-13 11:13:52 +0300

    update solution task1 from part3/Exception

--- 3.Exceptions/src/part3/task1/Solution1.java
+++ 3.Exceptions/src/part3/task1/Solution1.java
@@ -1,40 +1,28 @@
 package part3.task1;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Solution1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String expression;
         do {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                String expression = scanner.nextLine();
-                Pattern pattern = Pattern.compile("[0-9]+|[/*-+]");
-                Matcher matcher = pattern.matcher(expression);
-                ArrayList<String> array = new ArrayList<>();
+            expression = scanner.nextLine();
+        } while (!expression.matches("\\d+ []/*-+] \\d+"));
 
-                while (matcher.find()) {
-                    array.add(matcher.group());
-                }
-                double num1 = Double.parseDouble(array.get(0));
-                double num2 = Double.parseDouble(array.get(2));
-                double result = switch (array.get(1)) {
-                    case "*" -> num1 * num2;
-                    case "/" -> num1 / num2;
-                    case "+" -> num1 + num2;
-                    case "-" -> num1 - num2;
-                    default -> Double.NaN;
-                };
-                System.out.println(result);
-                break;
-            }
-            catch (Exception e){
-                System.out.println("Неверный ввод");
-            }
-        } while (true);
+        String[] elements = expression.split(" ");
+
+        double num1 = Double.parseDouble(elements[0]);
+        double num2 = Double.parseDouble(elements[2]);
+        double result = switch (elements[1]) {
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            default -> Double.NaN;
+        };
+        System.out.println(result);
     }
 }
 
