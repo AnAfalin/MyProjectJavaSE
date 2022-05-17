e4f5b6a18b862ab8dfe4685626d7d4a1bec018fc
Anastasia <an.afalin@yandex.ru>
2022-05-17 12:27:06 +0300

    update solution task1 from part1/RegularExpressions

--- 5.RegularExpressions/src/part1/Task1.java
+++ 5.RegularExpressions/src/part1/Task1.java
@@ -8,10 +8,21 @@ public class Task1 {
         //шаблон, ищущий все слова от 2 до 5 букв.
         String str = "text world bottle milk juice сахар 12345 sun one -20";
 
+
+        //решение с помощью pattern & matcher
         Pattern pattern = Pattern.compile("\\b[A-Za-zА-Яа-я0-9]{2,5}\\b");
         Matcher matcher = pattern.matcher(str);
         while (matcher.find()) {
             System.out.println(matcher.group());
+        }
+
+
+        //решение с помощью matches
+        String[] arr = str.split(" ");
+        for (String s : arr) {
+            if (s.matches("[A-Za-zА-Яа-я0-9]{2,5}")) {
+                System.out.println(s);
+            }
         }
     }
 }
