49cec5483e9f22d0c9a7aafdc971f1cb0fbea5c6
Anastasia <an.afalin@yandex.ru>
2022-05-20 17:48:34 +0300

    update solution task#7 from part1/collectionTreeSetTreeMap

--- 7.Collections/src/collectionTreeSetTreeMap/part1/task7/ConvertToArabic.java
+++ 7.Collections/src/collectionTreeSetTreeMap/part1/task7/ConvertToArabic.java
@@ -4,16 +4,17 @@ import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class ConvertToArabic {
+    private static final NavigableMap<Character, Integer> data = new TreeMap<>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
     public void convertToArabic(String number) {
-        NavigableMap<Character, Integer> data = new TreeMap<>() {{
-            put('I', 1);
-            put('V', 5);
-            put('X', 10);
-            put('L', 50);
-            put('C', 100);
-            put('D', 500);
-            put('M', 1000);
-        }};
 
         int result = 0;
         for (int i = 0; i < number.length(); i++) {
