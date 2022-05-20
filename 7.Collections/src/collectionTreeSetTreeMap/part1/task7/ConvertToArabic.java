6f7b903572d878a7a6f4dde091181a81fab99c0e
Anastasia <an.afalin@yandex.ru>
2022-05-20 17:46:56 +0300

    update solution task#7 from part1/collectionTreeSetTreeMap

--- 7.Collections/src/collectionTreeSetTreeMap/part1/task7/ConvertToArabic.java
+++ 7.Collections/src/collectionTreeSetTreeMap/part1/task7/ConvertToArabic.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class ConvertToArabic {
     public void convertToArabic(String number) {
-        NavigableMap<Character, Integer> dataRomanArabic = new TreeMap<>() {{
+        NavigableMap<Character, Integer> data = new TreeMap<>() {{
             put('I', 1);
             put('V', 5);
             put('X', 10);
@@ -18,12 +18,12 @@ public class ConvertToArabic {
         int result = 0;
         for (int i = 0; i < number.length(); i++) {
 
-            if (i == 0 || dataRomanArabic.get(number.charAt(i)) <= dataRomanArabic.get(number.charAt(i - 1))) {
-                result += dataRomanArabic.get(number.charAt(i));
+            if (i == 0 || data.get(number.charAt(i)) <= data.get(number.charAt(i - 1))) {
+                result += data.get(number.charAt(i));
             } else {
-                result += dataRomanArabic.get(number.charAt(i)) - 2 * dataRomanArabic.get(number.charAt(i - 1));
+                result += data.get(number.charAt(i)) - 2 * data.get(number.charAt(i - 1));
             }
         }
-        System.out.println("Число арабскими цифрами - " + number + "\nЧисло римскими цифрами - " + result);
+        System.out.println("Число римскими цифрами - " + number + "\nЧисло арабскими цифрами - " + result);
     }
 }
