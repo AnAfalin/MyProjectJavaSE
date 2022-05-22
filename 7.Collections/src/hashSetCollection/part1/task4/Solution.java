adb39a738d7411ed75a7c690cbb6f7ac7d7bf5c9
Anastasia <an.afalin@yandex.ru>
2022-05-22 22:44:39 +0300

    update solution task#4 from part1/hashSetCollection/Collections

--- 7.Collections/src/hashSetCollection/part1/task4/Solution.java
+++ 7.Collections/src/hashSetCollection/part1/task4/Solution.java
@@ -2,34 +2,20 @@ package hashSetCollection.part1.task4;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
-        String text = "Ночь, улица, фонарь, аптека, " +
-                "Бессмысленный и тусклый свет. " +
-                "Живи еще хоть четверть века — " +
-                "Всё будет так. Исхода нет." +
-                "Умрешь — начнешь опять сначала " +
-                "И повторится всё, как встарь: " +
-                "Ночь, ледяная рябь канала, " +
-                "Аптека, улица, фонарь.";
-        HashSet<Character> hashSet = new HashSet<>();
+        String text = "привет, мир.";
+        Map<Character, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < text.length(); i++) {
-            hashSet.add(text.charAt(i));
-        }
-
-        System.out.println(hashSet);
-
-        HashMap<Character, Integer> hashMap = new HashMap<>();
-        for (Character el:hashSet){
-            int count = 0;
-            for (int i = 0; i < text.length(); i++) {
-                if(el.equals(text.charAt(i))){
-                    count++;
-                }
+            if(hashMap.containsKey(text.charAt(i))){
+                hashMap.put(text.charAt(i), hashMap.get(text.charAt(i)) + 1);
+            }else {
+                hashMap.put(text.charAt(i), 1);
             }
-            hashMap.put(el, count);
         }
+
         System.out.println("Символ в слове=количество повторений в тексте\n" + text);
         System.out.println(hashMap);
     }
