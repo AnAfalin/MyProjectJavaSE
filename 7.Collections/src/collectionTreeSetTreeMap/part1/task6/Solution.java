6498d1e4017f2dd4838f04ea34593777574223ec
Anastasia <an.afalin@yandex.ru>
2022-05-22 23:22:26 +0300

    update solution task#6 from part1/collectionTreeSetTreeMap

--- 7.Collections/src/collectionTreeSetTreeMap/part1/task6/Solution.java
+++ 7.Collections/src/collectionTreeSetTreeMap/part1/task6/Solution.java
@@ -1,19 +1,39 @@
 package collectionTreeSetTreeMap.part1.task6;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Solution {
     public static void main(String[] args) {
         //поиск в TreeSet - O(log(n))
-        //поиск в HashSet - O(n)
+        //поиск в HashSet - O(1)
 
         int n = 16;
-        compareSearchSpeed(n);
+        Set<Integer> treeSet = new TreeSet<>();
+        Set<Integer> hashSet = new HashSet<>();
+        addSet(treeSet, 1000000);
+        addSet(hashSet, 1000000);
+        System.out.println("Поиск одного и того же элемента составляет ");
+        System.out.print("В реализации TreeSet интефейса Set: " + compareSearchSpeed(hashSet, 255_555) + " наносекунд\n");
+        System.out.print("В реализации HashSet интефейса Set: " + compareSearchSpeed(hashSet, 255_555) + " наносекунд\n");
     }
 
-    public static void compareSearchSpeed(int n){
-        double speedTreeSet = Math.log(n) / Math.log(2);
-        double speedHashSet = n;
-        System.out.println("Скорость поиска элемента в TreeSet = " + speedTreeSet);
-        System.out.println("Скорость поиска элемента в HashSet = " + speedHashSet);//10_000_000
-        System.out.println("Скорость поиска элемента в TreeSet быстрее поиска в HashSet в " + speedHashSet / speedTreeSet + " раз");
+    public static void addSet(Set<Integer> set, int count){
+        for (int i = 0; i < count; i++) {
+            set.add(i);
+        }
+    }
+
+    public static boolean contains(Set<Integer> set, int el){
+        return set.contains(el);
+    }
+
+    public static long compareSearchSpeed(Set<Integer> set, int el){
+        long begin = System.nanoTime();
+        contains(set, el);
+        long end = System.nanoTime();
+        return (end - begin);
     }
 }
