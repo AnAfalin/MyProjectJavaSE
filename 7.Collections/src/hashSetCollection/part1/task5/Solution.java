f9474f8ef1aff83e0b37135a6bfa2717bab4b6f9
Anastasia <an.afalin@yandex.ru>
2022-05-22 22:50:48 +0300

    update solution task#5 from part1/hashSetCollection/Collections

--- 7.Collections/src/hashSetCollection/part1/task5/Solution.java
+++ 7.Collections/src/hashSetCollection/part1/task5/Solution.java
@@ -1,24 +1,25 @@
 package hashSetCollection.part1.task5;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        Random random = new Random();
         for (int i = 0; i < 20; i++) {
-            list.add((int) (Math.random() * 9));
+            list.add(random.nextInt(100, 200));
         }
         Collections.sort(list);
         System.out.println("Исходный отсортированный список");
         System.out.println(list);
 
-        HashSet<Integer> hashSet = new HashSet<>(list);
+        Set<Integer> hashSet = new LinkedHashSet<>(list);
         System.out.println(hashSet);
+
         list.clear();
         list.addAll(hashSet);
+
+        System.out.println("Отсортированный список без дубликатов");
         System.out.println(list);
     }
 }
