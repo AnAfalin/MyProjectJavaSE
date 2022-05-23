5e2ad4dfb4aec2217279d95f5a03c1c667eb147f
Anastasia <an.afalin@yandex.ru>
2022-05-23 09:21:17 +0300

    update solution task#67 from part1/collectionTreeSetTreeMap

--- 7.Collections/src/collectionTreeSetTreeMap/part1/task7/Convert.java
+++ 7.Collections/src/collectionTreeSetTreeMap/part1/task7/Convert.java
@@ -3,6 +3,7 @@ package collectionTreeSetTreeMap.part1.task7;
 public class Convert {
     public static void main(String[] args) {
         new ConvertToArabic().convertToArabic("CCLVIII");
-        new ConvertToRoman().convertToRoman(999);
+        new ConvertToRoman().convertToRoman(258);
+        new ConvertToRoman1().convertToRoman(258);
     }
 }
--- 7.Collections/src/collectionTreeSetTreeMap/part1/task7/ConvertToRoman1.java
+++ 7.Collections/src/collectionTreeSetTreeMap/part1/task7/ConvertToRoman1.java
@@ -1,26 +1,22 @@
 package collectionTreeSetTreeMap.part1.task7;
 
 /*
-1 способ
+2 способ
  */
 
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
-public class ConvertToRoman {
+public class ConvertToRoman1 {
 
     public void convertToRoman(int number) {
         NavigableMap<Integer, String> data = new TreeMap<>() {{
             put(1, "I");
-            put(4, "IV");
             put(5, "V");
-            put(9, "IX");
             put(10, "X");
             put(50, "L");
-            put(90, "XC");
             put(100, "C");
             put(500, "D");
-            put(900, "CM");
             put(1000, "M");
         }};
         int numberArabic = number;
