c083057be5927230746ed275eb7dd422c5855e98
Anastasia <an.afalin@yandex.ru>
2022-05-24 14:02:13 +0300

    update solution task#4 from part1/additionalToolsCollections

--- 7.Collections/src/additionalToolsCollections/part1/task4/Solution.java
+++ 7.Collections/src/additionalToolsCollections/part1/task4/Solution.java
@@ -7,21 +7,31 @@ public class Solution {
         Integer[] array = new Integer[20];
         Random random = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt( 50);
+            array[i] = random.nextInt( 10);
             System.out.print(array[i] + " ");
         }
         System.out.println();
 
-        Queue<Integer> list = new PriorityQueue<>(List.of(array));
-
-        for (int i = 0; i < array.length; i++) {    //O(n)
-            array[i] = list.poll();                 //O(log(n))
-        }
+        Map<Integer, Integer> hashMap = new HashMap<>();
 
         for (Integer el:array) {
-            System.out.print(el + " ");
+            if(hashMap.containsKey(el)){
+                hashMap.put(el, hashMap.get(el) + 1);
+            }else {
+                hashMap.put(el, 1);
+            }
         }
 
-        //Arrays.sort(array); // O(n*log(n))
+        int index = 0;
+        Set<Map.Entry<Integer, Integer>> entries = hashMap.entrySet();
+        for (Map.Entry<Integer, Integer> el:entries) {
+            for (int i = 0; i < el.getValue(); i++) {
+                array[index] = el.getKey();
+                index++;
+            }
+        }
+
+        System.out.println(hashMap);
+        System.out.println(Arrays.toString(array));
     }
 }
