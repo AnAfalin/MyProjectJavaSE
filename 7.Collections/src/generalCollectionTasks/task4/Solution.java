b8fc34ff8af5d8b12eb9eb245c0fc97b85e3e946
Anastasia <an.afalin@yandex.ru>
2022-05-29 23:37:45 +0300

    update solution task#4 from generalCollectionTasks

--- 7.Collections/src/generalCollectionTasks/task4/Solution.java
+++ 7.Collections/src/generalCollectionTasks/task4/Solution.java
@@ -3,6 +3,5 @@ package generalCollectionTasks.task4;
 public class Solution {
     public static void main(String[] args) {
         new WorksOfTwoBanksMachine();
-        //new QueuesOfTwoBanksMachine();
     }
 }
