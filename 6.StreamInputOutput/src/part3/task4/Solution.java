6442857239206acc3f2a7d99524e02ccb9533089
Anastasia <an.afalin@yandex.ru>
2022-06-11 23:51:33 +0300

    update task4 from part3/StreamInputOutput

--- 6.StreamInputOutput/src/part3/task4/Solution.java
+++ 6.StreamInputOutput/src/part3/task4/Solution.java
@@ -1,24 +1,39 @@
 package part3.task4;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution {
-    private static final String[] fieldFile = new String[]{"people", "firstname", "lastname", "age", "job", "animals"};
+    private static final String[] fieldFile = new String[]{"people", "person", "firstname", "lastname", "age", "job", "animals", "animal"};
 
     public void saveXMLFile(Person[] people, String path) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path))) {
-            bufferedWriter.write("<root>\n");
+            bufferedWriter.write("<" + fieldFile[0] + ">\n");
             for (Person el : people) {
-                bufferedWriter.write("<" + fieldFile[0] + ">\n");
-                for (int i = 1; i < fieldFile.length; i++) {
+                bufferedWriter.write("<" + fieldFile[1] + ">\n");
+                for (int i = 2; i < fieldFile.length - 1; i++) {
                     bufferedWriter.write("\t<" + fieldFile[i] + ">");
-                    bufferedWriter.write(el.getElementPerson(fieldFile[i]));
-                    bufferedWriter.write("</" + fieldFile[i] + ">\n");
+                    String valueField = el.getElementPerson(fieldFile[i]);
+                    if(i == 6){
+                        bufferedWriter.write("\n");
+                        for (String animal:valueField.split(", ")) {
+                            bufferedWriter.write("\t\t<" + fieldFile[7] + ">") ;
+                            bufferedWriter.write(animal);
+                            bufferedWriter.write("</" + fieldFile[7] + ">\n");
+                        }
+                        bufferedWriter.write("\t</" + fieldFile[i] + ">\n");
+                    }else {
+                        bufferedWriter.write(valueField);
+                        bufferedWriter.write("</" + fieldFile[i] + ">\n");
+                    }
+
                 }
-                bufferedWriter.write("</" + fieldFile[0] + ">\n");
+                bufferedWriter.write("</" + fieldFile[1] + ">\n");
             }
-            bufferedWriter.write("</root>");
+            bufferedWriter.write("</" + fieldFile[0] + ">\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,23 +42,26 @@ public class Solution {
     public Person[] downloadXMLFile(String path) {
         Person[] peopleRead = new Person[3];
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("6.StreamInputOutput/src/part3/task4/XML_data.xml"))) {
-            String[] fieldPerson = new String[5];
+            List<String> fieldPerson = new ArrayList<>();
             String str;
-            int i = 0;
+            int k = 0;
             int countPerson = 0;
             while ((str = bufferedReader.readLine()) != null) {
-
-                if (str.contains("/people")) {
-                    Person person = new Person(fieldPerson[0], fieldPerson[1], Integer.parseInt(fieldPerson[2]), fieldPerson[3], (fieldPerson[4]).split(", "));
+                if (str.contains("/person")) {
+                    String[] animals = new String[fieldPerson.size() - k - 4];
+                    int j = 0;
+                    for (int i = k + 4; i < fieldPerson.size(); i++, j++) {
+                        animals[j] = fieldPerson.get(i);
+                    }
+                    Person person = new Person(fieldPerson.get(k), fieldPerson.get(k+1), Integer.parseInt(fieldPerson.get(k+2)), fieldPerson.get(k+3), animals);
                     peopleRead[countPerson] = person;
                     countPerson++;
-                    i = 0;
+                    k = fieldPerson.size();
                     continue;
                 }
-                if (!str.contains("root") && !str.contains("people")) {
+                if (!str.contains("people") && !str.contains("person") && !str.contains("animals")) {
                     str = str.substring(str.indexOf(">") + 1, str.lastIndexOf("<"));
-                    fieldPerson[i] = str;
-                    i++;
+                    fieldPerson.add(str);
                 }
             }
         } catch (IOException e) {
