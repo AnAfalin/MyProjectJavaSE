38ede1b7411e30d5e3a3f314e715c95056d2925b
Anastasia <an.afalin@yandex.ru>
2022-06-12 12:37:26 +0300

    add solution task6 from part4/StreamInputOutput

---
+++ 6.StreamInputOutput/src/part4/task6/Solution.java
@@ -0,0 +1,28 @@
+package part4.task6;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        List<String> list = List.of("Java", "Pascal", "JavaScript", "Python", "Assembler", "Swift");
+        try {
+            Files.createFile(Path.of("6.StreamInputOutput/src/part4/task6/text.txt"));
+            Files.write(Path.of("6.StreamInputOutput/src/part4/task6/text.txt"), list, StandardOpenOption.CREATE_NEW);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(Files.exists((Path.of("6.StreamInputOutput/src/part4/task6/text.txt")))){
+                Files.delete(Path.of("6.StreamInputOutput/src/part4/task6/text.txt"));
+            }
+            Files.write(Path.of("6.StreamInputOutput/src/part4/task6/text.txt"), list);
+        }
+
+
+    }
+}
