79fcb0ab08ca579b67205812158fc5f7deb968e4
Anastasia <an.afalin@yandex.ru>
2022-06-12 12:29:05 +0300

    add solution task5 from part4/StreamInputOutput

---
+++ 6.StreamInputOutput/src/part4/task5/Solution.java
@@ -0,0 +1,32 @@
+package part4.task5;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //запись с помощью потоков ввода-вывода
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("6.StreamInputOutput/src/part4/task5/text.txt"))){
+            Scanner scanner = new Scanner(System.in);
+            bufferedWriter.write(scanner.nextLine() + "\n");
+            bufferedWriter.write(scanner.nextLine() + "\n");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //запись с помощтю класса Files
+        if(!Files.exists(Path.of("6.StreamInputOutput/src/part4/task5/text.txt"))) {
+            Files.createFile(Path.of("6.StreamInputOutput/src/part4/task5/text.txt"));
+        }
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine() + "\n";
+        String s2 = scanner.nextLine() + "\n";
+        Files.writeString(Path.of("6.StreamInputOutput/src/part4/task5/text.txt"), s1, StandardOpenOption.APPEND);
+        Files.writeString(Path.of("6.StreamInputOutput/src/part4/task5/text.txt"), s2, StandardOpenOption.APPEND);
+
+    }
+}
