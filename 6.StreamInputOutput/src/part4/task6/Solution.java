357caa0d2e9f11b2e98e31c94cf3baaba2f1d4a2
Anastasia <an.afalin@yandex.ru>
2022-06-13 18:10:36 +0300

    add solution task7 from part4/StreamInputOutput

--- 6.StreamInputOutput/src/part4/task6/Solution.java
+++ 6.StreamInputOutput/src/part4/task6/Solution.java
@@ -1,7 +1,6 @@
 package part4.task6;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
