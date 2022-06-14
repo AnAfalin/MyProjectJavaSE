1938c559d02d7bb5f0a10fc78e0397001f64d6fb
Anastasia <an.afalin@yandex.ru>
2022-06-14 19:04:19 +0300

    update solution task1 from part1/LambdaExpression

--- 8.LambdaExpression/src/part1/task1/Solution.java
+++ 8.LambdaExpression/src/part1/task1/Solution.java
@@ -9,14 +9,12 @@ public class Solution {
         List<Integer> list = new ArrayList<>(List.of(100, -10, 10, 200));
         System.out.println(list);
 
-
         Predicate<Integer> filter = new Predicate<Integer>() {
             @Override
             public boolean test(Integer integer) {
                 return !(integer >= 10 && integer <= 100);
             }
         };
-
 
         list.removeIf(el -> (el < 10 || el > 100));
 
