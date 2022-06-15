7cae6c6134223810d172bb43247d3f60b0ea5222
Anastasia <an.afalin@yandex.ru>
2022-06-15 09:07:33 +0300

    add solution task4 from part1/LambdaExpression

---
+++ 8.LambdaExpression/src/part1/task4/Solution.java
@@ -0,0 +1,29 @@
+package part1.task4;
+
+import java.util.Comparator;
+
+public class Solution {
+    public static void main(String[] args) {
+//        Comparator<String> comparator = new Comparator<String>() {
+//            @Override
+//            public int compare(String str1, String str2) {
+//                if(str1.length() > str2.length()){
+//                    return 1;
+//                }else if(str1.length() < str2.length()){
+//                    return -1;
+//                }else {
+//                    return 0;
+//                }
+//            }
+//        };
+
+//        Comparator<String> comparator = new Comparator<String>() {
+//            @Override
+//            public int compare(String str1, String str2) {
+//                return Integer.compare(str1.length(), str2.length());
+//            }
+//        };
+
+        Comparator<String> comparator = (str1, str2) -> Integer.compare(str1.length(), str2.length());
+    }
+}
