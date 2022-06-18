2a4488051fd04325b9282cd4d605c70acdef2b04
Anastasia <an.afalin@yandex.ru>
2022-06-18 23:34:26 +0300

    update solution task7 from part4/StreamInputOutput

--- 6.StreamInputOutput/src/part4/task7/Solution.java
+++ 6.StreamInputOutput/src/part4/task7/Solution.java
@@ -5,11 +5,11 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        String path = "6.StreamInputOutput/src/part4/task6/sourceFile.txt";
+        String path = "6.StreamInputOutput/src/part4/task7/sourceFile.txt";
         RandomAccessFile randomAccessFile = new RandomAccessFile(path, "rw");
 
         int prevNum = Integer.parseInt(Character.toString((char) randomAccessFile.read()));
-        System.out.println("Предыдущее число " + prevNum);
+//        System.out.println("Предыдущее число " + prevNum);
         int currentNum;
 
         int lastNum = -1;
@@ -17,15 +17,15 @@ public class Solution {
         while ((c = randomAccessFile.read()) != -1) {
             lastNum = Integer.parseInt(Character.toString((char) c));
         }
-        System.out.println("Последнее число " + lastNum);
+//        System.out.println("Последнее число " + lastNum);
 
 
-        System.out.println("Положение курсора " + randomAccessFile.getFilePointer());
+//        System.out.println("Положение курсора " + randomAccessFile.getFilePointer());
         randomAccessFile.seek(1);
-        System.out.println("Положение курсора " + randomAccessFile.getFilePointer());
+//        System.out.println("Положение курсора " + randomAccessFile.getFilePointer());
         while ((c = randomAccessFile.read()) != -1) {
             currentNum = Integer.parseInt(Character.toString((char) c));
-            System.out.println(currentNum);
+//            System.out.println(currentNum);
             if (currentNum - prevNum == 1) {
                 prevNum = currentNum;
             } else {
