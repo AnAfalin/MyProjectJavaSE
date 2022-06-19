5707b0007f82c15615d7774d218bebe5116d517f
Anastasia <an.afalin@yandex.ru>
2022-06-19 13:21:05 +0300

    update solution task#4 from part2/LambdaExpressions

--- 8.LambdaExpressions/src/part2/task4/Solution.java
+++ 8.LambdaExpressions/src/part2/task4/Solution.java
@@ -1,5 +1,6 @@
 package part2.task4;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Solution {
@@ -8,11 +9,19 @@ public class Solution {
         int num2 = 4;
 
         Function<Integer, String> function = (n) -> Integer.toString(n);
-        String res = concatStringNumber(function, num1, num2);
-        System.out.println(res);
+        String res1 = function1(function, num1, num2);
+        System.out.println(res1);
+
+        BiFunction<Integer, Integer, String> biFunction = (n1, n2) -> n1 + Integer.toString(n2);
+        String res2 = function2(biFunction, num1, num2);
+        System.out.println(res2);
     }
 
-    public static String concatStringNumber(Function<Integer, String> function, int number1, int number2){
+    public static String function1(Function<Integer, String> function, int number1, int number2){
         return function.apply(number1) + function.apply(number2);
+    }
+
+    public static String function2(BiFunction<Integer, Integer, String> biFunction, int number1, int number2){
+        return biFunction.apply(number1, number2);
     }
 }
