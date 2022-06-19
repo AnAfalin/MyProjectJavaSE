64fa40d1381e170406ad8c5a40728c8af1dbe96a
Anastasia <an.afalin@yandex.ru>
2022-06-19 09:33:02 +0300

    update solution task3 from part1/algorithms/Additional

--- 11.Additional/src/algorithms/part1/task3/Solution.java
+++ 11.Additional/src/algorithms/part1/task3/Solution.java
@@ -2,31 +2,28 @@ package algorithms.part1.task3;
 
 public class Solution {
     public static int binarySearch(int[] array, int el) {
-        return binarySearch(array, array.length - 1, el);
-    }
-
-    private static int binarySearch(int[] array, int toIndex, int el) {
-        int index = (array.length) / 2 - 1;
-        while (el != array[index]) {
-            if(el > array[index]){
-                if(toIndex <= array.length - 1){
-                    index = toIndex;
-                }else {
-                    index = index + 1;
-                }
+        int lowIndex = 0;
+        int highIndex = array.length - 1;
+        while (lowIndex <= highIndex){
+            int middle = lowIndex + (highIndex - lowIndex) / 2;
+            if(el < array[middle]){
+                highIndex = middle - 1;
+            }else if(el > array[middle]){
+                lowIndex = middle + 1;
             }else {
-                toIndex = index - 1;
-                index = index / 2;
+                return middle;
             }
         }
-        return index;
+        return -1;
     }
 
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-        int el = 9;
+        int el1 = 19;
+        int el2 = 9;
 
-        System.out.println(binarySearch(array, el));
+        System.out.println(binarySearch(array, el1));
+        System.out.println(binarySearch(array, el2));
     }
 }
 
