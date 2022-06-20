c535118fbc7bd5457530653af145e5e3487db29c
Anastasia <an.afalin@yandex.ru>
2022-06-20 19:49:05 +0300

    update solution task#2 from part1/Stream

--- 9.Stream/src/part1/task2/Solution.java
+++ 9.Stream/src/part1/task2/Solution.java
@@ -2,17 +2,19 @@ package part1.task2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Solution {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 1_000; i++) {
-            list.add(i);
-        }
 
-        Stream<Integer> stream = list.stream().filter(el -> el == 0 || (Math.sqrt(el) / (int) Math.sqrt(el) == 1.0));
-        stream.forEach(el -> System.out.print(el + " "));
+        IntStream.rangeClosed(0, 1_000)
+                        .forEach(list::add);
+
+        list
+                .stream()
+                .filter(el -> el == 0 || (Math.sqrt(el) / (int) Math.sqrt(el) == 1.0))
+                .forEach(el -> System.out.print(el + " "));
 
     }
 }
