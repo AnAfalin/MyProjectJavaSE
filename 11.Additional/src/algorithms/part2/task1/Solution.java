fa1da9e0ce8890ef9b5ed5fe6a1776401537a59d
Anastasia <an.afalin@yandex.ru>
2022-06-22 12:48:29 +0300

    add solution task#1 from part2/algorithms/Additional

---
+++ 11.Additional/src/algorithms/part2/task1/Solution.java
@@ -0,0 +1,39 @@
+package algorithms.part2.task1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str = "Java. Now is June. Now is Summer";
+        Character symbol = firstSymbolString(str, getMapSymbol(str));
+        System.out.println(symbol);
+
+    }
+
+    //O(n)
+    public static Character firstSymbolString(String str, Map<Character, Integer> mapSymbol) {
+        Character symbol;
+        for (int i = 0; i < str.length(); i++) {        //O(n)
+            symbol = str.charAt(i);                     //O(1)
+            if (mapSymbol.get(symbol) == 1) {           //O(1)
+                return symbol;
+            }
+        }
+        return null;
+    }
+
+
+    public static Map<Character, Integer> getMapSymbol(String str) {
+        Map<Character, Integer> mapSymbol = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (mapSymbol.containsKey(str.charAt(i))) {
+                mapSymbol.put(str.charAt(i), mapSymbol.get(str.charAt(i)) + 1);
+            } else {
+                mapSymbol.put(str.charAt(i), 1);
+            }
+        }
+        return mapSymbol;
+    }
+}
