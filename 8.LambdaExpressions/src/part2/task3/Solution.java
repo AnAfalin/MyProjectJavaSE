3a425be955a4d2ecc0ffb7fc2e8013af2af2323a
Anastasia <an.afalin@yandex.ru>
2022-06-22 10:56:53 +0300

    update solution task#3 from part2/LambdaExpressions

--- 8.LambdaExpressions/src/part2/task3/Solution.java
+++ 8.LambdaExpressions/src/part2/task3/Solution.java
@@ -4,20 +4,42 @@ import java.util.function.*;
 
 public class Solution {
     public static void main(String[] args) {
-        //UnaryOperator<Double> sqrt = (n) -> Math.sqrt(n);
-        UnaryOperator<Double> sqrt = Math::sqrt;
+        /*
+        Виды ссылок на методы:
+            Ссылка на статический метод через название класса;
+            Ссылка на нестатический метод через экземпляр класса;
+            Ссылка на нестатический метод через название класса;
+            Ссылка на конструктор.
+         */
 
-        //Consumer<Integer> print = (n) -> System.out.println(n);
-        Consumer<Integer> print = System.out::println;
+        //Ссылка на статический метод через название класса
+        BiFunction<Integer, Integer, Double> pow = Math::pow; //(x, y) -> Math.pow(x, y)
+        int number = 5;
+        int degree = 2;
+        double powNumber = pow.apply(number, degree);
 
-        //UnaryOperator<Double> log10 = (n) -> Math.log10(n);
-        UnaryOperator<Double> log10 = Math::log10;
+        //Ссылка на нестатический метод через экземпляр класса
+        //аргумент из лямбда-выражения (x) не изменяется -> можно заменить лямбду на ссылку на метод
+        String str = "Java";
+        Function<String, Integer> function = String::length;    //x -> x.length()
 
-        //BiFunction<Integer, Integer, Double> pow = (n1, n2) -> Math.pow(n1, n2);
-        BiFunction<Integer, Integer, Double> pow = Math::pow;
+        //Ссылка на нестатический метод через название класса
+        //параметр лямбда (x) используется только для вызова метода из класса String -> можно заменить на ссылку на метод:
+        Function<Integer, Double > unaryOperator = Integer::doubleValue;    //x -> x.doubleValue()
 
-        //Supplier<Double> getRandomDouble = () -> Math.random();
-        Supplier<Double> getRandomDouble = Math::random;
+        //Ссылка на конструктор
+        //вызывается определенный конструктор -> можно заменить на ссылку
+        BiFunction<String, String, WordRusEng> newWord = WordRusEng::new; //(rus, eng) -> new WordRusEng(rus, eng)
 
+    }
+}
+
+class WordRusEng{
+    String rusWord;
+    String engWord;
+
+    public WordRusEng(String rusWord, String engWord) {
+        this.rusWord = rusWord;
+        this.engWord = engWord;
     }
 }
