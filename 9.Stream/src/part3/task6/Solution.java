ec474cf408911b08e8652860c7331527c073f769
Anastasia <an.afalin@yandex.ru>
2022-07-07 00:00:27 +0300

    add solution task#6 from part3/Stream

--- 9.Stream/src/part3/task6/Solution.java
+++ 9.Stream/src/part3/task6/Solution.java
@@ -1,12 +1,12 @@
 package part3.task6;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Solution {
+
     public static void main(String[] args) {
         List<String> listString = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("9.Stream/src/part3/task6/Applicants.txt"))){
@@ -17,5 +17,58 @@ public class Solution {
             ex.printStackTrace();
         }
         System.out.println(listString);
+
+
+        /*поток только имен*/
+        Stream<String> nameStream = listString
+                .stream()
+                .map(list -> list.split(" "))
+                .map(string -> string[1]);
+
+        nameStream.forEach(System.out::println);
+
+
+        /*поток имен и фамилий*/
+        Stream<String> nameSurnameStream = listString
+                .stream()
+                .map(list -> list.substring(list.indexOf(" ") + 1, list.lastIndexOf(" ")));
+
+        nameSurnameStream.forEach(System.out::println);
+
+
+        /*поток студентов*/
+        Stream<Student> studentStream = listString
+                .stream()
+                .map(string -> string.split(" "))
+                .map(el -> new Student(
+                        Integer.parseInt(el[0]),
+                        el[1],
+                        el[2],
+
+                        Arrays.stream(el[3].substring(1, el[3].length() - 1).split(","))
+                                .map(s -> Integer.parseInt(s))
+                                .collect(Collectors.toList())));
+
+        studentStream.forEach(System.out::println);
+
+    }
+}
+
+class Student {
+    private int ID;
+    private String name;
+    private String surname;
+    private List<Integer> marks;
+
+    public Student(int ID, String name, String surname, List<Integer> marks) {
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return ID + " " + name + " " + surname + " " + marks;
     }
 }
