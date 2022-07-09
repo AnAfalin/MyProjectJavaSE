2eb4261bc91009a829007ec8ea7ff7531adae623
Anastasia <an.afalin@yandex.ru>
2022-07-09 23:26:25 +0300

    add solution task#13 from part1/DateTimeAPI

---
+++ 10.DateTimeAPI/src/part1/task13/Solution.java
@@ -0,0 +1,55 @@
+package part1.task13;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Map<LocalTime, Integer> scheduleTrain = getScheduleTrain();
+
+        System.out.println("Время отправления = Время в пути");
+        scheduleTrain
+                .forEach((key, value) -> System.out.println(key + "=" + value));
+    }
+
+    public static Map<LocalTime, Integer> getScheduleTrain() {
+        Map<LocalTime, Integer> map = new LinkedHashMap<>();
+        LocalTime departure = LocalTime.of(0, 0, 0);
+        LocalTime arrive;
+        int min = 0;
+
+        while (min < Duration.ofDays(1).toMinutes()) {
+
+            int departureMin = new Random().nextInt(40, 60);
+            departure = departure.plusMinutes(departureMin);
+            min += departureMin;
+            int travelMin = new Random().nextInt(15, 40);
+            arrive = departure.plusMinutes(travelMin);
+            min += travelMin;
+            map.put(departure, travelMin);
+            departure = arrive;
+        }
+        return map;
+    }
+/*
+    public static void  methodVar(){
+        LocalTime current = LocalTime.of(0, 0);
+
+        SortedSet<LocalTime> timetable = new TreeSet<>();
+        int totalMinutes = 0;
+        do{
+            timetable.add(current);
+            int rand = new Random().nextInt(40, 60);
+            current = current.plusMinutes(rand);
+            totalMinutes += rand;
+        }while (totalMinutes < Duration.ofDays(1).toMinutes());
+
+        timetable.
+                forEach(System.out::println);
+    }
+*/
+
+}
+
