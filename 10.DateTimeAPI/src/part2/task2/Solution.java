3a0f34216129b081e6693d8a4e70e93d0d20c04d
Anastasia <an.afalin@yandex.ru>
2022-07-15 22:07:14 +0300

    update solution task#2 from part2/DateTimeAPI

--- 10.DateTimeAPI/src/part2/task2/Solution.java
+++ 10.DateTimeAPI/src/part2/task2/Solution.java
@@ -1,12 +1,14 @@
 package part2.task2;
 
-import java.time.LocalTime;
+import java.time.Duration;
 import java.time.Year;
+import java.time.temporal.ChronoUnit;
 
 public class Solution {
     public static void main(String[] args) {
         Year year = Year.now();;
-        int seconds = year.length() * ((LocalTime.MAX.getHour()) + 1) * ((LocalTime.MAX.getMinute()) + 1) * ((LocalTime.MAX.getSecond()) + 1);
+
+        long seconds = Duration.of(Year.of(year.getValue()).length(), ChronoUnit.DAYS).toSeconds();
         System.out.println("В " + year + " году " + seconds + " сек.");
     }
 }
