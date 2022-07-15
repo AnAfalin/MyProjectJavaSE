cea13820ad14e55f3d93e4ab160fe3f68b719121
Anastasia <an.afalin@yandex.ru>
2022-07-15 21:17:31 +0300

    update solution task#1 from part1/AnnotationReflection

--- 12.AnnotationReflection/src/part1/task4/Test.java
+++ 12.AnnotationReflection/src/part1/task4/Test.java
@@ -3,11 +3,9 @@ package part1.task4;
 public class Test {
     @AnnotationDate(2022)
     public void method() {
-
     }
 
     @AnnotationDate(year = 2022)
     public void method1() {
-
     }
 }
