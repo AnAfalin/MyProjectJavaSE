dda2000044ec2f6eb76e33a51e98fbef99cf0f91
Anastasia <an.afalin@yandex.ru>
2022-07-19 00:03:03 +0300

    add solution task#3 from part3\AnnotationReflection

---
+++ 12.AnnotationReflection/src/part3/task3/Solution.java
@@ -0,0 +1,35 @@
+package part3.task3;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Class<StringJava> stringJavaClass = StringJava.class;
+
+        Field name = stringJavaClass.getDeclaredField("name");
+
+        name.setAccessible(true);
+        System.out.println(name.get(stringJavaClass));
+
+        name.set(stringJavaClass, "IT");
+        System.out.println(name.get(stringJavaClass));
+
+        Method doubleString = stringJavaClass.getDeclaredMethod("doubleString");
+        doubleString.setAccessible(true);
+        String result = (String) doubleString.invoke(stringJavaClass);
+        System.out.println(result);
+    }
+}
+
+class StringJava {
+    private static String name = "Java";
+
+    public static String doubleString(){
+        return name + " " + name;
+    }
+}
+
+
+
+
